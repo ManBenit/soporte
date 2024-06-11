2dfb7d8286357aa8da5eadcac61c0821303898ee
edgar-leon <edgar950308@hotmail.com>
2024-06-11 12:06:00 -0600

    Se ajusta el llamado al servicio y estilos para apinubeServices

--- src/components/ApiNubeService.vue
+++ src/components/ApiNubeService.vue
@@ -1,40 +1,65 @@
 <template>
-  <div>
+  <div class="contenedor">
+
     <button @click="callExternalService" class="btn btn-primary">Api Nube Services</button>
-    <div v-if="success === 'true'">
-        <button type="button" class="btn btn-success">Success</button>
-    </div>
-    <div v-if="failed === 'true'">
-            <button type="button" class="btn btn-danger">Failed</button>
-    </div>
+
+    <Modal :visible="showModal" @close="showModal = false">
+      <h2>Servicios de Api Nube</h2>
+
+        <div v-for="item in titulos">
+            <p>{{ item.title }}</p>
+            <div v-if="success === 'true'">
+                    <button  type="button" class="btn btn-success">Success</button>
+                  </div>
+                  <div v-if="failed === 'true'">
+                    <button type="button" class="btn btn-danger">Failed</button>
+                  </div>
+          </div>
+
+
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './ModalE.vue';
+
 export default {
+  components: {
+      Modal
+    },
   data() {
     return {
+      showModal: false,
       success : 'false',
       failed : 'false',
-    };
+      titulos: [],
+      }
   },
   methods: {
     async callExternalService() {
+      this.showModal = true;
       try {
-        const response = await fetch('http://localhost:8080/api/roaming/tarifasTerrestre');
+        const response = await fetch('http://localhost:8080/api/roaming/consultar-servicios');
         const data = await response.json();
-        console.log(response);
-        if(response.status==200){
+        console.log(data);
+
+        for (var i = 0; i < data.length; i++) {
+          console.log(data[i].serviceName, data[i].statusCode);
+          this.titulos[i] = {title: data[i].serviceName};
+
+          console.log(this.titulos)
+          if(data[i].statusCode==200){
             this.success = 'true';
             this.failed = 'false';
-        }else{
+          }else{
             this.success = 'false';
             this.failed = 'true';
+          }
         }
-
       } catch {
         this.success = 'false';
-         this.failed = 'true';
+        this.failed = 'true';
       }
     },
   },
@@ -42,6 +67,9 @@ export default {
 </script>
 
 <style scoped>
+.contenedor{
+    padding: 150px;
+}
 button {
   margin-bottom: 10px;
 }
