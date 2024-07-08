1396224ea699028ad2a44d477d235f84a749d8f3
edgar-leon <edgar950308@hotmail.com>
2024-07-08 11:53:16 -0600

    Se agrega accicion al boton succes y tooltip a failed

--- src/components/ApiNubeService.vue
+++ src/components/ApiNubeService.vue
@@ -3,15 +3,17 @@
     <button @click="callExternalService" class="btn btn-primary">Servicios de Api Nube</button>
 
     <Modal :visible="showModal" @close="showModal = false">
-      <center><h2>Servicios de Api Nube</h2></center>
+      <h2>Servicios de Api Nube</h2>
       <hr>
       <div v-for="(service, index) in services" :key="index">
         <p>{{ service.titulo }}</p>
         <div v-if="service.status">
-          <button type="button" class="btn btn-success">Succes</button>
+          <a :href="generateJsonLink(service.body)" target="_blank">
+            <button type="button" class="btn btn-success">Success</button>
+          </a>
         </div>
         <div v-else>
-          <button title="your-tooltip-here" type="button" class="btn btn-danger">Failed</button>
+          <button ref="failedButton" :title="extractErrorMessage(service.body)" type="button" class="btn btn-danger" data-bs-toggle="tooltip">Failed</button>
         </div>
       </div>
     </Modal>
@@ -40,15 +42,27 @@ export default {
         this.services = data.map(service => ({
           titulo: service.serviceName,
           status: service.statusCode === 200,
-          body: service.body
+          body: service.body,
         }));
       } catch (error) {
-        console.error("Error al llamar al servicio: ", error);
-        this.services = data.map(service => ({
-                  titulo: service.serviceName,
-                  status: service.statusCode === 400,
-                  body: error
-                }));
+        this.services = [{
+          titulo: 'Error',
+          status: false,
+          body: error.message,
+          errorMessage: 'Error en la invocacion del servicio'
+        }];
+      }
+    },
+    generateJsonLink(jsonContent) {
+      const blob = new Blob([JSON.stringify(jsonContent, null, 2)], { type: 'application/json' });
+      return URL.createObjectURL(blob);
+    },
+    extractErrorMessage(body) {
+      const match = body.match(/\d{3}\s\w+\s\w+/);
+      if (match) {
+        return match[0];
+      } else {
+        return 'Error al obtener el codigo de respuesta';
       }
     }
   }
@@ -56,8 +70,8 @@ export default {
 </script>
 
 <style scoped>
-h2{
-
+h2 {
+  text-align: center;
 }
 .contenedor {
   padding: 150px;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "@/assets/css/bootstrap.min.css";
-import "@/assets/js/bootstrap.bundle.min.js";
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@/assets/css/bootstrap.min.css';
+import '@/assets/js/bootstrap.bundle.min.js';
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).mount('#app');
